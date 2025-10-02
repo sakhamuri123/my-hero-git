13f937bbbf74c1ef1dbd7ade10b6a1028bdb3b66
sakhamuri123 <31386201+sakhamuri123@users.noreply.github.com>
2025-10-02 18:19:45 +0530

    Delete password.py

--- password.py
+++
@@ -1,8 +0,0 @@
-password = "admin"
-
-pwd = input("Enter the password:")
-
-while pwd != password:
-    print("Incorrect password, try again.")
-    pwd = input("Enter the password:")
-print("Access granted.")
